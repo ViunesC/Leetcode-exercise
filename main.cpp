391b92ff7298b4a55b01855e70ebec62d9174c1e
ViunesC <mrfusking233@gmail.com>
2024-02-29 09:13:09 -0500

    update

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Easy/q9.h"
+#include "Easy/q13.h"
+
+int main() {
+    // std::cout << q9::isPalindrome(121) << std::endl;
+    // std::cout << q13::romanToInt("XIX") << std::endl;
+    return 0;
+}
