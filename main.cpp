08a0b8de73cd43237be4c1df7ec827e4bfcd33a3
ViunesC <mrfusking233@gmail.com>
2024-03-12 10:44:13 -0400

    update

--- main.cpp
+++ main.cpp
@@ -7,14 +7,5 @@
 int main() {
     // std::cout << q9::isPalindrome(121) << std::endl;
     // std::cout << q13::romanToInt("XIX") << std::endl;
-    std::vector<std::string> vrts;
-    vrts.push_back("Apple");
-    vrts.push_back("Baha");
-    vrts.push_back("Appolo");
-    vrts.push_back("Application");
-    vrts.push_back("Charlie");
-    vrts.push_back("Behemorth");
-    vrts.push_back("Banana");
-    q14::longestCommonPrefix(vrts);
     return 0;
 }
